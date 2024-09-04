77075e62f0c2065a799561f1a79c56664ac28cb8
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 12:32:45 -0300

    componentes tailwind

---
+++ compCSS/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  
+  return (
+    <>
+      <div className="h-28 w bg-pink-700"></div>
+      <div className="box h-screen w-60 flex flex-col p-20 bg-green-300 items-center justify-around">
+        <div className="flex h-40 w-40  bg-pink-700"></div>
+        <div className="flex h-40 w-40  bg-pink-700"></div>
+        <div className="flex h-40 w-40  bg-pink-700"></div>
+      </div>
+    </>
+  )
+}
+
+export default App
